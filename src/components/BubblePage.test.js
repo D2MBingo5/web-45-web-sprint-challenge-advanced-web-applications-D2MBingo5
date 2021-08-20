09dc7d7ef01ba966aa3345fdc960b08752c08b6e
Leu Family <romanianfobjr@gmail.com>
2021-08-20 17:08:30 -0500

    troubles trying to work with async in tests

--- src/components/BubblePage.test.js
+++ src/components/BubblePage.test.js
@@ -4,16 +4,46 @@ import MutationObserver from 'mutationobserver-shim';
 import { render, screen} from "@testing-library/react";
 import BubblePage from './BubblePage';
 
+import fetchColorService from '../services/fetchColorService'
+jest.mock('../services/fetchColorService')
+
 test("Renders without errors", ()=> {
     render(<BubblePage />)
+    // this no longer passes with the introduction of jest.mock
 });
 
 test("Renders appropriate number of colors passed in through mock", async ()=> {
     //Keep in mind that our service is called on mount for this component.
+    fetchColorService.mockResolvedValueOnce({
+        colors: [
+            {
+                color: "aliceblue",
+                code: {
+                  hex: "#f0f8ff",
+                },
+                id: 1,
+              },
+              {
+                color: "limegreen",
+                code: {
+                  hex: "#99ddbc",
+                },
+                id: 2,
+              },
+              {
+                color: "aqua",
+                code: {
+                  hex: "#00ffff",
+                },
+                id: 3,
+              }
+        ]
+    })
+
     // arrange
     render(<BubblePage />)
     // act
-    let colors = screen.queryAllByTestId('color')
+    let colors = await screen.findAllByTestId('color')
     // assert
-    expect(colors).toHaveLength(11)
+    expect(colors).toHaveLength(3)
 });
