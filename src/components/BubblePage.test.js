da0fc985020eeb6e57bfd4851856695be1c5290d
Leu Family <romanianfobjr@gmail.com>
2021-08-20 16:59:32 -0500

    unmounted component memory leak console.error

--- src/components/BubblePage.test.js
+++ src/components/BubblePage.test.js
@@ -5,9 +5,15 @@ import { render, screen} from "@testing-library/react";
 import BubblePage from './BubblePage';
 
 test("Renders without errors", ()=> {
-    
+    render(<BubblePage />)
 });
 
 test("Renders appropriate number of colors passed in through mock", async ()=> {
     //Keep in mind that our service is called on mount for this component.
+    // arrange
+    render(<BubblePage />)
+    // act
+    let colors = screen.queryAllByTestId('color')
+    // assert
+    expect(colors).toHaveLength(11)
 });
